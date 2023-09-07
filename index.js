a146ce46fce32f3142a160295e6847887fde4205
Paola Sofía Mendoza <117321550+paosofiam@users.noreply.github.com>
2023-09-07 01:51:46 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-/*function calculadora(event) {
-event.preventDefault();
-  var variable1 = document.getElementById("input1").value;
-  var variable2 = document.getElementById("input2").value;
-
-  var variable3 = parseInt(variable1) + parseInt(variable2);
-  console.log(variable3);
-  const resultado = document.getElementById("resultado");
-  console.log(resultado);
-  resultado.innerHTML = variable3;
-}*/
-
-
 const botoncito = document.getElementById("botoncito");
-botoncito.addEventListener('click', function (e) {
+botoncito.addEventListener("click", function (e) {
   e.preventDefault();
   var variable1 = document.getElementById("input1").value;
   var variable2 = document.getElementById("input2").value;
@@ -21,5 +8,5 @@ botoncito.addEventListener('click', function (e) {
   console.log(variable3);
   const resultado = document.getElementById("resultado");
   console.log(resultado);
-  resultado.innerHTML = variable3;
+  resultado.innerHTML = "el resultado es: " + variable3;
 });
